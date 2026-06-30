f3ce88dbf3cb5c7a64a68d0a27ee383d35ff12ae
Shahid <mjshahid001@gmail.com>
2026-06-30 14:27:33 +0100

    RELEASE: v1.0 Release

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -12,6 +12,8 @@ public class App
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Paul"));
+        System.out.println(sayHelloToSomeone("Shahid"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
